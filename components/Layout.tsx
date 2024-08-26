6695510a27a0a325c803be9be01755d4abb2b145
Honza Brázdil <jbrazdil@redhat.com>
2024-08-26 20:17:17 +0200

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -85,7 +85,7 @@ export const Layout: React.FC<Props> = ({
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
-            <div className="min-h-screen bg-barovia bg-no-repeat bg-cover bg-center bg-fixed">
+            <div className="min-h-screen bg-gradient-to-br from-[#2e253fb3] to-[#40224fbf] bg-barovia bg-no-repeat bg-cover bg-center bg-fixed">
                 {/* NAVBAR */}
                 <div
                     className={clsx(
